838090a4b06ed1c6f00951a704745fc79a7a84df
victordsgamorim <victordsgamorim@gmail.com>
2021-03-21 21:48:29 +0000

    Adiciona lista de imagens para função k-means

--- multi_kmeans.m
+++ multi_kmeans.m
@@ -1,4 +1,47 @@
-function seg = multi_kmeans(I, qtdTresh)
+
+clear all;
+
+n_th = 10;
+
+diretoria = 'imagens_segmentacao';
+arquivos = dir(fullfile(diretoria,'*.jpg'));
+
+num_fig = 25;
+num_img = 1;
+num_fig_seg = 1;
+
+for i = 1: length(arquivos)
+    filename = fullfile(diretoria, arquivos(i).name);
+    im = imread(filename);
+    
+    figure(num_fig); imshow(im); title('Original');
+    im = double(rgb2gray(im));
+    
+    for j = 1 : n_th
+     
+        seg = multi_kmeans(im, j, num_fig_seg, num_img);
+ 
+        
+%         metricas do professor
+        intensity  = max(max(seg));
+        sim = ssim(seg* (255/intensity), im);
+        mean_sq_err = immse(seg*(255/intensity), im);
+        multi_sim = multissim(seg*(255/intensity), im);
+        
+        if(num_img < 10)
+            num_img = num_img + 1;
+        else
+            num_img = 1;
+        end 
+        
+    end
+    
+    num_fig = num_fig + 1;
+    num_fig_seg = num_fig_seg + 1;
+end
+
+
+function seg = multi_kmeans(I, qtdTresh, num_fig_seg, num_img)
    
     %[L,Centers] = imsegkmeans(I,qtdTresh);
     %B = labeloverlay(I,L);
@@ -25,9 +68,9 @@ function seg = multi_kmeans(I, qtdTresh)
     end
     clustered = sortidx(clustered);
     
-    figure(2);
-    imshow(clustered, []);
+    figure(num_fig_seg); subplot (2, 5, num_img);  imshow(clustered, []);
+
     title('Labeled Image');
-    seg = clustered
+    seg = clustered;
     
 end
