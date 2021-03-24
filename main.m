5c99b59007f6a7d7626e22f79f8c8a5e6205b665
Gabriel Carneiro <gabri14el@gmail.com>
2021-03-24 20:24:57 +0000

    correcoes

--- main.m
+++ main.m
@@ -35,12 +35,17 @@ for i = 1: length(arquivos)
     hold off;
     
     %itera sobre os threshs
+    ssim_ = zeros(2, length(tresh));
+    mse_ = zeros(2, length(tresh));
+    multi_sim_ = zeros(2, length(tresh));
+    psnr_ = zeros(2, length(tresh));
+    
     for x = 1:length(tresh)
         
         fprintf('calculando para thresh %s \n', int2str(tresh(x)));
         
         %calcula os métodos
-        kmeans = multi_kmeans(gray, tresh(x));
+        kmeans = multi_kmeans(gray, tresh(x)+1);
         otsu = multi_otsu(gray, tresh(x));
         
         %converte a matriz de 0 a 255
@@ -61,6 +66,12 @@ for i = 1: length(arquivos)
         psnr_kmeans = psnr(kmeans,gray);
         psnr_otsu = psnr(otsu,gray);
         
+        ssim_(1, x) = ssim_otsu;  ssim_(2, x) = ssim_kmeans; 
+        multi_sim_(1, x) = multi_sim_otsu; multi_sim_(2, x) = multi_sim_kmeans;
+        mse_(1, x) = mean_sq_err_otsu; mse_(2, x) = mean_sq_err_kemans; 
+        psnr_(1, x) = psnr_otsu; psnr_(2, x) = psnr_kmeans;
+        
+        
         
         
         %plota resultado
@@ -77,20 +88,36 @@ for i = 1: length(arquivos)
         subplot(n, m, x+1); imshow(kmeans); title(str_title);
         
         hold off;
+        
+        
+        
     end
     
     %saveas(1,strcat('results/otsu_', arquivos(i).name));
     %saveas(2,strcat('results/kmeans_', arquivos(i).name));
-    print(1, '-dpng', strcat('results/otsu_', arquivos(i).name), '-r300')
-    print(2, '-dpng', strcat('results/kmeans_', arquivos(i).name), '-r300')
+    figure(3);
+    hold on;
+    subplot(2, 2, 1); xticks(tresh); ylim([0 1]); plot(tresh, ssim_(1,:), tresh, ssim_(2, :)); legend('otsu', 'kmeans'); title('Structural similarity'); xlabel('threshold'); ylabel('valor da métrica');
+    subplot(2, 2, 2); xticks(tresh); ylim([0 1]); plot(tresh, multi_sim_(1,:), tresh, multi_sim_(2, :)); legend('otsu', 'kmeans'); title('Multi-scale structural similarity'); xlabel('threshold'); ylabel('valor da métrica');
+    subplot(2, 2, 3); xticks(tresh); ylim([0 1]); plot(tresh, mse_(1,:), tresh, mse_(2, :)); legend('otsu', 'kmeans'); title('Mean-squared error'); xlabel('threshold'); ylabel('valor da métrica');
+    subplot(2, 2, 4); xticks(tresh); ylim([0 1]); plot(tresh, psnr_(1,:), tresh, psnr_(2, :)); legend('otsu', 'kmeans'); title('Peak signal-to-noise ratio'); xlabel('threshold'); ylabel('valor da métrica');
+    hold off;
+    print(1, '-dpng', strcat('results/otsu_', arquivos(i).name), '-r300');
+    print(2, '-dpng', strcat('results/kmeans_', arquivos(i).name), '-r300');
+    print(3, '-dpng', strcat('results/metrics_', arquivos(i).name), '-r300');
     %pause();
     
     %limpa as figuras casos elas estejam abertas
     if ishandle(1)
         clf(1);
     end
+    
     if ishandle(2)
         clf(2);
+    end
+    
+    if ishandle(3)
+        clf(3);
     end
 end
 
